8ed27fe80c7a277ec7874d6618edd79f874b3768
rrrrroger <chanrogerck@gmail.com>
2017-07-18 01:26:59 +0800

    gggggg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 $("html").removeClass("no-js");
 
-function a(c,d,e,f,j,l,o,m,n,o,p,q) {
+function a(c,d,e,f,g,h,j,l,o,m,n,o,p) {
     b = c.clone(),
+    $searchBlk = f.clone(),
+    $logo = g.clone(),
+    $lang = h.clone(),
     $mobiSearchBlk = j.clone(),
     d.hover(function() {
         var a = $(this);
@@ -25,13 +28,13 @@ function a(c,d,e,f,j,l,o,m,n,o,p,q) {
     f.on("click", function() {
         $(this).find(".search__box").focus()
     }),
-    l.append(b, m),
+    l.append($searchBlk, b, m),
     $("body").append(l),
     o.on("click", function(a) {
         a.preventDefault(),
         $("html").addClass("animating no-scroll"),
         o.addClass("is-open")
-    }),        
+    }),
     m.prepend($mobiSearchBlk),
     n.on("click", function(a) {
         a.preventDefault(),
@@ -44,10 +47,11 @@ function a(c,d,e,f,j,l,o,m,n,o,p,q) {
           , d = new RegExp("menu__","gi");
         b.attr("class", c.replace(d, "mobi-menu__"))
     }),
+    $('.close-mobi-menu-container').prepend($logo,$("<ul>"+$lang+"</ul>").html()),
     $(".mobi-panel li.has-sub > .mobi-menu__link").each(function(a) {
         var b = $(this)
           , c = b.parents("ul");
-        c.length > 2 || b.find(".js-toggle-mobi-submenu").length || b.append('<span class="btn-toggle-mobi-submenu js-toggle-mobi-submenu" />')
+        c.length > 2 || b.find(".js-toggle-mobi-submenu").length || b.append('<span class="btn-toggle-mobi-submenu js-toggle-mobi-submenu icon-chevron-thin" />')
     }),
     $(document).on("click", ".js-toggle-mobi-submenu", function(a) {
         var b = $(this)
@@ -56,34 +60,34 @@ function a(c,d,e,f,j,l,o,m,n,o,p,q) {
         a.preventDefault(),
         a.stopPropagation(),
         d.hasClass("is-active") ? (d.removeClass("is-active"),
-        c.velocity("slideUp", {
-            duration: p,
-            easing: q
+        c.slideUp({
+            duration: p
         })) : (d.addClass("is-active"),
-        c.velocity("slideDown", {
-            duration: p,
-            easing: q
+        c.slideDown({
+            duration: p
         }))
     })
 }
 
 $(function () {
+    //mobile menu
     var b, c = $(".menu__list--lv1"), d = $(".menu__item--lv1"), e = ($(".menu__link--lv1"),
-    $(".menu__list--lv2"), 
-    $(".menu")), f = $(".search"), j = $(".search-blk").clone(), l = $('<nav class="mobi-panel visible-sm-block visible-xs-block visible-md-block"><div class="close-mobi-menu-container"><button class="btn btn--close-mobi-menu icon-close js-close-mobi-menu" type="button">'+ "</button></div></nav>"), m = $('<ul class="tools tools--mobi"></ul>'), n = l.find(".js-close-mobi-menu"), o = $(".js-open-mobi-menu"), p = 300, q = "ease-out";
-    a(c,d,e,f,j,l,o,m,n,o,p,q);
+    $(".menu__list--lv2"),
+    $(".menu")), f = $(".search"), g = $(".page-logo"), h = $(".tools__item--lang"), j = $(".search-blk").clone(), l = $('<nav class="mobi-panel visible-sm-block visible-xs-block visible-md-block"><div class="close-mobi-menu-container"><button class="btn btn--close-mobi-menu icon-close js-close-mobi-menu" type="button">'+ "</button></div></nav>"), m = $('<ul class="tools tools--mobi"></ul>'), n = l.find(".js-close-mobi-menu"), o = $(".js-open-mobi-menu"), p = 300;
+    a(c,d,e,f,g,h,j,l,o,m,n,o,p);
+
+    //parallax
+    var s = skrollr.init();
 });
 
 
 $('nav .has-sub').on("mouseover focus", function(){
     $(this).addClass("is-active");
-});
-
-$('nav .has-sub').on("mouseleave blur", function(){
+}).on("mouseleave blur", function(){
     $(this).removeClass("is-active");
 });
 
-$('.icon-search').on("click", function() {
+$('.icon-search').on("click focus", function() {
     $(this).parent().find(".search__box").focus()
 })
 
@@ -95,3 +99,15 @@ $(window).on('load resize', function() {
         //'top' : headerHeight
     })
 });
+
+$(window).on('scroll resize load', function() {
+    //var distance = 350;
+	var distancePercentage = 0.1
+    if ($(window).scrollTop() > $(window).height() * 0.7 - 85) {
+		//console.log("window scroll: " + $(window).height() * 0.5 + " :  YES");
+      $('header').addClass('scrolled');
+    }
+    else {
+      $('header').removeClass('scrolled');
+    }
+ });
